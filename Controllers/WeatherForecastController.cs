156c8b4f9e6a911bdbc6a5d596cfb7cb78b850ae
mathmartins2 <matheusgleiciel@gmail.com>
2024-05-10 16:23:38 -0300

    solving docker and docker compose

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "PICAlize",
+        "PICALIZE 2",
     };
 
     private readonly ILogger<WeatherForecastController> _logger;

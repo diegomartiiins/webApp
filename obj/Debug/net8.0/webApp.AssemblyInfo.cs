28a1b3470c28439676da4918a0fbcb154dc6b147
diegomartiiins <diego.adcm99@gmail.com>
2024-05-10 16:37:34 -0300

    update

--- obj/Debug/net8.0/webApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/webApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+067054ecd025a08ddd43e059fee13764f427ffe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4f655a7a6f4bb8bcb9b1a6223216e614737775")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
